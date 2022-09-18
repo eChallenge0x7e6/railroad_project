55407c6fe8e92901e674bce067e26f018a50cd5b
eChallenge <echallenge0x7e6@gmail.com>
2022-09-18 11:28:18 +0900

    2022/09/18 Nakayama:メイン基板とモジュール基板の基本的なフローをコーディングした。（動作未確認）

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,30 +1,106 @@
+//ライブラリの宣言
 #include <Wire.h>
 
+//マクロの定義
 #define m0_addr (0x40)
 #define m1_addr (0x41)
 #define m2_addr (0x42)
+#define m3_addr (0x43)
 
-// the setup function runs once when you press reset or power the board
+//グローバル変数の宣言
+union{
+  uint8_t data;
+  struct{
+    uint8_t reserved : 4;
+    uint8_t on_rail : 3;
+    uint8_t output : 1;
+  }rail_sts;
+}com;//I2C受信用データ変数
+union{
+  uint8_t data;
+  struct{
+    char reserved : 6;
+    char directry : 1;
+    char output : 1;
+  }cmd;
+}tx;//I2C送信用データ変数
+
+//プロトタイプ宣言
+bool check(void);
+void i2c_m0_tx(uint8_t data); //モジュールm0指令用関数
+void i2c_m1_tx(uint8_t data); //モジュールm1指令用関数
+void i2c_m2_tx(uint8_t data); //モジュールm2指令用関数
+void i2c_m3_tx(uint8_t data); //モジュールm3指令用関数
+
+/**
+ * 初期化関数
+ * 起動後、最初に一度だけ処理される。
+ */
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600);
-  Wire.begin();
+  Serial.begin(9600); //デバッグ用シリアル出力設定
+
+  Wire.begin();       //I2C初期設定
+
+  //各変数の初期化
+  com.data = 0;
+  tx.data = 0;
+
+  //路線状態の確認
+  check();
+
+  //運行開始
+  tx.cmd.output = 1;  //発車指令
+  i2c_m0_tx(tx.data); //指令送信
 }
 
+/**
+ * ループ関数
+ * 初期化関数の後、常に繰り返し動作する。
+ */
 void loop() {
-  Wire.requestFrom(m0_addr, 1);
-  while (Wire.available()){
-    char c = Wire.read();
-    Serial.print(c);
+  //区間0の状態確認
+  Wire.requestFrom(m0_addr, 1);   //区間状態を受け取る
+  while (Wire.available()){       //受信データ数の確認
+    com.data = Wire.read();       //データ読み取り
+    Serial.print(com.data);
   }
-  static unsigned char test = 0;
-  if(test<0xff){
-    test++;
+  //区間0へ指令
+  if(com.rail_sts.on_rail!=0){
+    tx.cmd.output = 0;
   } else {
-    test = 0;
+    tx.cmd.output = 1;
   }
-  Wire.beginTransmission(m0_addr);
-  Wire.write(test);
-  Wire.endTransmission();
-  delay(1000);
+  i2c_m0_tx(tx.data);   //指令送信
+  
+  delay(100);           //待機時間(msec)
+}
+
+bool check(void){
+  //線路内の列車の有無を確認
+  //残列車の誘導
+  return true;
+}
+
+void i2c_m0_tx(uint8_t data){
+  Wire.beginTransmission(m0_addr);  //送信アドレス設定
+  Wire.write(data);                 //送信データ
+  Wire.endTransmission();           //送信終了
+}
+
+void i2c_m1_tx(uint8_t data){
+  Wire.beginTransmission(m1_addr);  //送信アドレス設定
+  Wire.write(data);                 //送信データ
+  Wire.endTransmission();           //送信終了
+}
+
+void i2c_m2_tx(uint8_t data){
+  Wire.beginTransmission(m2_addr);  //送信アドレス設定
+  Wire.write(data);                 //送信データ
+  Wire.endTransmission();           //送信終了
+}
+
+void i2c_m3_tx(uint8_t data){
+  Wire.beginTransmission(m3_addr);  //送信アドレス設定
+  Wire.write(data);                 //送信データ
+  Wire.endTransmission();           //送信終了
 }
--- src/module/module.ino
+++ src/module/module.ino
@@ -1,63 +1,127 @@
-#include <Wire.h>
+//ライブラリの宣言
+#include <Wire.h>       //I2Cライブラリ
+#include <MsTimer2.h>   //タイマーライブラリ
 
-#define address (0x40)
-#define relay 8
-#define on HIGH
-#define off LOW
-#define sen_a A3 //10bit
-#define sen_b A2 //10bit
+//マクロの定義
+#define ADDRESS (0x41)      //I2C用モジュールアドレス
+#define RELAY   (8)         //リレー出力ピン番号
+#define ON      (HIGH)      //リレー用トランジスタのアクティブ極性
+#define OFF     (LOW)       //リレー用トランジスタのネガティブ極性
+#define SENS_A  (A3)        //センサA入力ピン番号
+#define SENS_B  (A2)        //センサB入力ピン番号
+#define ACT_THR (100)       //センサの閾値（アクティブ）
+#define NEG_THR (500)       //センサの閾値（ネガティブ）
+#define SENS_PERIOD   (10)  //列車検出の閾時間(msec)
 
-//Status Register bit map
-#define EXIST_ON_RAIL (0)
+//グローバル変数の宣言
+union{
+  uint8_t data;
+  struct{
+    uint8_t reserved : 4;
+    uint8_t on_rail : 3;
+    uint8_t output : 1;
+  }rail_sts;
+}com;                     //I2C送信用データ変数
+union{
+  uint8_t data;
+  struct{
+    char reserved : 6;
+    char directry : 1;
+    char output : 1;
+  }cmd;
+}rcv;                     //I2C受信用データ変数
+int dir_flow = 1;         //列車進行方向変数　右回り：1, 左回り：-1
+unsigned int r_sens_a = 0;//センサAの列車検出値
+unsigned int r_sens_b = 0;//センサBの列車検出値
+bool detect_a = false;    //列車検出フラグA
+bool detect_b = false;    //列車検出フラグB
 
-//Command Register bit map
-#define SAFE_SIG (0)
-
-// the setup function runs once when you press reset or power the board
+/**
+ * 初期化関数
+ * 起動後、最初に一度だけ処理される。
+ */
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(relay, OUTPUT);
-  //pinMode(sen_a,INPUT_PULLUP);
-  //pinMode(sen_b,INPUT_PULLUP);
-  Serial.begin(9600);
-  Wire.begin(address);
-  Wire.onRequest(requestEvent);
-  Wire.onReceive(receiveEvent);
+  pinMode(RELAY, OUTPUT);       //リレー出力ピン設定
+
+  Serial.begin(9600);           //デバッグ用シリアル出力設定
+
+  Wire.begin(ADDRESS);          //I2C設定
+  Wire.onRequest(RequestEvent); //I2Cリクエスト関数設定
+  Wire.onReceive(ReceiveEvent); //I2C受信関数設定
+
+  //各変数の初期化
+  com.data = 0;
+  rcv.data = 0;
+  com.rail_sts.on_rail = 0;
+  com.rail_sts.output = ON;
 }
 
-unsigned char cmd_regist = 0;
-unsigned char sts_regist = 0;
-unsigned int val_sen_a = 0;
-unsigned int val_sen_b = 0;
+/**
+ * ループ関数
+ * 初期化関数の後、常に繰り返し動作する。
+ */
 void loop() {
-  static int on_rail_num = 0;
-  val_sen_a = analogRead(sen_a);
-  val_sen_b = analogRead(sen_b);
-  if(val_sen_a<100){
-    on_rail_num++;
-  }else if(val_sen_a>300){
-    on_rail_num--;
-  }
-  if(on_rail_num==0){
-    sts_regist &= ~(1<<EXIST_ON_RAIL);
-  } else {
-    sts_regist |= 1<<EXIST_ON_RAIL;
-  }
+  //loop関数内のローカル変数の宣言
+  static long on_sens_a = 0;    //センサAの検出タイミング
+  static long on_sens_b = 0;    //センサBの検出タイミング
+  static int on_rail_num = 0;   //区間内の列車数
+
+  r_sens_a = analogRead(SENS_A);//センサ読み取り
+  r_sens_b = analogRead(SENS_B);//センサ読み取り
+
+  //センサAの処理
+  if(!detect_a && r_sens_a<ACT_THR){      //列車の検出
+    detect_a = true;                      //検出フラグA ON
+    on_sens_a = millis();                 //検出時間記録
+  }else if(detect_a && r_sens_a>NEG_THR){ //列車の通過
+    detect_a = false;                     //検出フラグA OFF
+    if(millis()-on_sens_a > SENS_PERIOD){ //列車の通過時間確認
+      on_rail_num += dir_flow;            //区間内の列車数の算出
+    }//else Nothing to do
+  }//else Nothing to do
+
+  //センサBの処理
+  if(!detect_b && r_sens_b<ACT_THR){      //列車の検出
+    detect_b = true;                      //検出フラグB ON
+    on_sens_b = millis();                 //検出時間記録
+  }else if(detect_b && r_sens_b>NEG_THR){ //列車の通過
+    detect_b = false;                     //検出フラグB OFF
+    if(millis()-on_sens_b > SENS_PERIOD){ //列車の通過時間確認
+      on_rail_num -= dir_flow;            //区間内の列車数の算出
+    }//else Nothing to do
+  }//else Nothing to do
+
+  //区間状態の更新
+  com.rail_sts.on_rail = abs(on_rail_num);
 }
 
-void requestEvent(){
-  Wire.write(sts_regist);
-  Serial.print("Get Request\n");
+/**
+ * I2C送信関数
+ * リクエストに応え、区間状態を送信する。
+ */
+void RequestEvent(){
+  Wire.write(com.data);//データ送信
+  Serial.print("Sent data : 0x%02x\n", com.data);
 }
 
-void receiveEvent(int num){
-  while (0 < Wire.available()){
-    cmd_regist = Wire.read();
-    Serial.print(cmd_regist);
+/**
+ * I2C受信関数
+ * 受け取った命令を実行する。
+ */
+void ReceiveEvent(int num){
+  while(Wire.available() > 0){//受信データ数の確認
+    rcv.data = Wire.read();//データ受信
+    Serial.print("Get data : 0x%02x", rcv.data);
   }
-  if(cmd_regist&(1<<SAFE_SIG)){
-    digitalWrite(relay, on);
-  } else {
-    digitalWrite(relay, off);
+  if(rcv.cmd.output == 1){//受信データの読み取り
+    com.rail_sts.output = ON;
+  } else{
+    com.rail_sts.output = OFF;
+  }
+  digitalWrite(RELAY, rcv.cmd.output);//レールに出力
+  if(rcv.cmd.directry == 0){//進行方向の設定
+    dir_flow = 1;
+  } else{
+    dir_flow = -1;
   }
 }
